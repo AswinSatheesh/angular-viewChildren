3f274b3f7629cc4a682b2b43001d9882c3d41426
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2024-05-26 19:36:24 +0530

    ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewChildren,QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ANGULAR-VIEW-CHILDREN';
+  fullName : string = '';
+  @ViewChildren('inputEle') inputElements : QueryList<ElementRef>
+
+  show(){
+    let name = '';
+    this.inputElements.forEach((el) =>{
+      console.log(el.nativeElement.value);
+      name += el.nativeElement.value + ' ';
+    })
+    this.fullName = name.trim();
+  }
+  
 }
